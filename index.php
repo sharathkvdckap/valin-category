494dc42695de18f49b26c0566323cbebef7a0308
sharathkv <sharathkv@dckap>
2022-04-18 18:16:16 +0530

    create deactivated subcat manufacturers

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ curl_setopt_array($curl, [
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{ \n  \"parent_id\": 229,\n  \"name\": \"Heating\",\n  \"description\": \"<p>We offer a wide variety of products perfect for relaxing</p>\",\n  \"views\": 1050,\n  \"sort_order\": 3,\n  \"page_title\": \"Heating\",\n  \"search_keywords\": \"string\",\n  \"meta_keywords\": [\n    \"string\"\n  ],\n  \"meta_description\": \"string\",\n  \"layout_file\": \"default\",\n  \"is_visible\": true,\n  \"default_product_sort\": \"use_store_settings\",\n  \"image_url\": \"https://cdn11.bigcommerce.com/s-ca10qrhzok/product_images/uploaded_images/productdefault.gif\",\n  \"custom_url\": {\n    \"url\": \"/deactivated-categories/heating\",\n    \"is_customized\": true\n  }\n}",
+  CURLOPT_POSTFIELDS => "{ \n  \"parent_id\": 229,\n  \"name\": \"Manufacturers\",\n  \"description\": \"\",\n  \"views\": 1050,\n  \"sort_order\": 3,\n  \"page_title\": \"Manufacturers\",\n  \"search_keywords\": \"\",\n  \"meta_keywords\": [\n    \"\"\n  ],\n  \"meta_description\": \"\",\n  \"layout_file\": \"default\",\n  \"is_visible\": true,\n  \"default_product_sort\": \"use_store_settings\",\n  \"image_url\": \"https://cdn11.bigcommerce.com/s-ca10qrhzok/product_images/uploaded_images/productdefault.gif\",\n  \"custom_url\": {\n    \"url\": \"/deactivated-categories/manufacturers\",\n    \"is_customized\": true\n  }\n}",
   CURLOPT_HTTPHEADER => [
     "Accept: application/json",
     "Content-Type: application/json",
