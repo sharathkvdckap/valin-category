b0753c5e4d507b5811bf6e4198a45208cba343ae
sharathkv <sharathkv@dckap>
2022-04-18 16:45:59 +0530

    create category

--- index.php
+++ index.php
@@ -1,12 +1,32 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Welcome to First Page</h1>
-</body>
-</html>
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+  CURLOPT_URL => "https://api.bigcommerce.com/stores/ca10qrhzok/v3/catalog/categories",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "{\n  \"id\": 0,\n  \"parent_id\": 2,\n  \"name\": \"Bath\",\n  \"description\": \"<p>We offer a wide variety of products perfect for relaxing</p>\",\n  \"views\": 1050,\n  \"sort_order\": 3,\n  \"page_title\": \"Bath\",\n  \"search_keywords\": \"string\",\n  \"meta_keywords\": [\n    \"string\"\n  ],\n  \"meta_description\": \"string\",\n  \"layout_file\": \"category.html\",\n  \"is_visible\": true,\n  \"default_product_sort\": \"use_store_settings\",\n  \"image_url\": \"https://cdn8.bigcommerce.com/s-123456/product_images/d/fakeimage.png\",\n  \"custom_url\": {\n    \"url\": \"/shoes\",\n    \"is_customized\": true\n  }\n}",
+  CURLOPT_HTTPHEADER => [
+    "Accept: application/json",
+    "Content-Type: application/json",
+    "X-Auth-Token: l7l97641xflq4r4yxrhnx61esy9ujdd"
+  ],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+
+?>
