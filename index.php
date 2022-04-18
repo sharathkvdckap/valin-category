0ea8f9427e0ddb2d053271172df1fb178e3e2d7b
sharathkv <sharathkv@dckap>
2022-04-18 17:50:39 +0530

    create new product category

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ curl_setopt_array($curl, [
   CURLOPT_TIMEOUT => 30,
   CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
   CURLOPT_CUSTOMREQUEST => "POST",
-  CURLOPT_POSTFIELDS => "{ \n  \"parent_id\": 24,\n  \"name\": \"Deactivated Categories\",\n  \"description\": \"<p>We offer a wide variety of products perfect for relaxing</p>\",\n  \"views\": 1050,\n  \"sort_order\": 3,\n  \"page_title\": \"Deactivated Categories\",\n  \"search_keywords\": \"string\",\n  \"meta_keywords\": [\n    \"string\"\n  ],\n  \"meta_description\": \"string\",\n  \"layout_file\": \"default.html\",\n  \"is_visible\": true,\n  \"default_product_sort\": \"use_store_settings\",\n  \"image_url\": \"https://cdn11.bigcommerce.com/s-ca10qrhzok/product_images/uploaded_images/productdefault.gif\",\n  \"custom_url\": {\n    \"url\": \"/catalog/deactivated-categories\",\n    \"is_customized\": true\n  }\n}",
+  CURLOPT_POSTFIELDS => "{ \n  \"parent_id\": 24,\n  \"name\": \"New Products\",\n  \"description\": \"<p>We offer a wide variety of products perfect for relaxing</p>\",\n  \"views\": 1050,\n  \"sort_order\": 3,\n  \"page_title\": \"New Products\",\n  \"search_keywords\": \"string\",\n  \"meta_keywords\": [\n    \"string\"\n  ],\n  \"meta_description\": \"string\",\n  \"layout_file\": \"default\",\n  \"is_visible\": true,\n  \"default_product_sort\": \"use_store_settings\",\n  \"image_url\": \"https://cdn11.bigcommerce.com/s-ca10qrhzok/product_images/uploaded_images/productdefault.gif\",\n  \"custom_url\": {\n    \"url\": \"/catalog/new-products\",\n    \"is_customized\": true\n  }\n}",
   CURLOPT_HTTPHEADER => [
     "Accept: application/json",
     "Content-Type: application/json",
